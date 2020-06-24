cad5d497ae9a6d27297ea34b663888f1e6a416c3
Harold Jennett <haroldjennett@outlook.com>
2020-06-24 18:58:19 -0400

    Finalized project ver 1

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const gridContain = document.querySelector('.grid-contain');
 
-makeRow();
+makeGrid(16);
 
-function makeRow() {
+function makeGrid(gridNum) {
 
     let i;
-    for (i = 0; i < 16; i++) {
+    for (i = 0; i < gridNum; i++) {
 
         let gridRow = document.createElement('div');
         gridRow.classList.add('grid-row');
 
         let j;
-        for (j = 0; j < 16; j++) {
+        for (j = 0; j < gridNum; j++) {
 
             const gridDiv = document.createElement('div'); //make single div
             gridDiv.classList.add("grid-div"); // add class
@@ -20,25 +20,38 @@ function makeRow() {
 
         gridContain.appendChild(gridRow);
     };
+    const allGridDivs = document.querySelectorAll(".grid-div");
+
+    allGridDivs.forEach(addDivClick);
+
+    function addDivClick(divClick) {
+        divClick.addEventListener('mouseover', addColor);
+    }
+
+    function addColor(e) {
+        this.classList.add('add-color');
+        // let randomColor = Math.floor(Math.random()*16777215).toString(16);
+        // this.style.backgroundColor = `#${randomColor}`;
+    }
 }
 
-const allGridDivs = document.querySelectorAll(".grid-div");
-allGridDivs.forEach(addDivClick);
 
-function addDivClick(divClick) {
-    divClick.addEventListener('mouseover', addColor);
-}
-
-function addColor(e) {
-    this.classList.add('add-color');
-}
 
 const clear = document.querySelector(".clear");
+
 clear.addEventListener('click', clearGrid);
 
 function clearGrid() {
-    let colorGrid = document.querySelectorAll(".add-color");
-    colorGrid.forEach(colored => {
-        colored.classList.remove('add-color');
+    let removeGrids = document.querySelectorAll('.grid-row');
+    removeGrids.forEach(isGrid => {
+
+        gridContain.removeChild(isGrid);
+
     });
+    let gridSize = prompt('How many blocks per side of grid?');
+    makeGrid(gridSize);
+    // let colorGrid = document.querySelectorAll(".add-color");
+    // colorGrid.forEach(colored => {
+    //     colored.classList.remove('add-color');
+    // });
 }
